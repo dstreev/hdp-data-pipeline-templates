1e3d353d015bd194342215a0047f48da486c429b
David W. Streever <david@streever.com>
2015-12-27 16:38:04 -0500

    New Workflow to call a pig script, followed by a HiveQL Script.

---
+++ bin/sftp_rm_to_hdfs_expect.sh
@@ -0,0 +1,142 @@
+#!/bin/bash
+
+#######
+# Get file from an SFTP site using an 'expect' script and put it on HDFS.
+#
+# Feed file format:
+#   SFTP_SOURCE_DIR FILE_PREFIX HDFS_TARGET_DIR
+#   /Data/Outbound/Curves USD_TREASURY_ /user/wre/wrelib/curves
+#######
+
+DATE_WITHOUT_DASHES=false
+YESTERDAY=false
+NO_SRC_DATE=false
+APPEND_DATE_TO_TARGET=false
+DELETE_SRC=false
+
+
+while [ $# -gt 0 ]; do
+  case "$1" in
+    --ssh_env)
+      shift
+      SSH_ENV=$1
+      shift
+      ;;
+    --feed_file)
+      shift
+      FEED_FILE=$1
+      shift
+      ;;
+    --no_src_date)
+      NO_SRC_DATE=true
+      shift
+      ;;
+    --append_date_to_target)
+      APPEND_DATE_TO_TARGET=true
+      shift
+      ;;
+    --delete_src)
+      DELETE_SRC=true
+      shift
+      ;;
+    --date)
+      shift
+      DT=$1
+      shift
+      ;;
+    --yesterday)
+      shift
+      YESTERDAY=true
+      ;;
+    --expect)
+      shift
+      EXPECT_SCRIPT=$1
+      shift
+      ;;
+    --date_without_dashes)
+        shift
+        DATE_WITHOUT_DASHES=true
+        ;;
+    --check)
+      CHECK="yes"
+      shift
+      ;;
+    --help)
+      echo "Usage: $0 --date <date in yyyyMMdd> --help"
+      exit -1
+      ;;
+    *)
+      break
+      ;;
+  esac
+done
+
+if [ "$DT" == "" ]; then
+    DT=`date +%Y%m%d`
+fi
+
+if [ "${YESTERDAY}" == "true" ]; then
+    DT=`./prev_work_day.sh ${DT}`
+fi
+
+if [ -f ${SSH_ENV} ]; then
+    . ${SSH_ENV}
+else
+    echo "Can't locate environment file ${SSH_ENV}"
+    exit -1
+fi
+
+if [ "${DATE_WITHOUT_DASHES}" == "true" ]; then
+    GOOD_DT="${DT}"
+else
+    GOOD_DT="${DT:0:4}-${DT:4:2}-${DT:6:2}"
+fi
+
+
+exec< $FEED_FILE
+
+while read line ; do
+    # Create a temp directory
+    BASE_OUTPUT_DIR=$( mktemp -d /tmp/transfer_XXXXXXX )
+
+    ar=( $line )
+
+    # Skip Comment lines
+    s1=$line
+    s2=#
+
+    # Skip Commented lines
+    if [ "${s1:0:${#s2}}" != "$s2" ]; then
+
+        SRC_DIR="${ar[0]}"
+        SRC_FILE="${ar[1]}_${DT}*.csv"
+        TARGET_DIR="${ar[2]}"
+
+        echo "Getting  ${SRC_FILE} files from sftp"
+
+        expect -f ${EXPECT_SCRIPT} ${SSH_PORT} ${SSH_USER} ${SSH_HOST} ${SRC_DIR} ${SRC_FILE} ${BASE_OUTPUT_DIR} ${SSH_PASSWORD}
+
+        echo "Push file(s) ${SRC_FILE} to HDFS Directory: ${TARGET_DIR}"
+        hdfs dfs -copyFromLocal -f ${BASE_OUTPUT_DIR}/*.* $TARGET_DIR
+
+
+        if [ "${APPEND_DATE_TO_TARGET}" == "false" ]; then
+            TARGET_FULL_FILE=${FULL_FILE}
+        else
+            TARGET_FULL_FILE=${FULL_FILE}_${GOOD_DT}
+        fi
+
+
+        scp -i ${SSH_KEY_FILE} -P ${SSH_PORT} ${SSH_USER}@${SSH_HOST}:${SOURCE_DIR}/${FULL_FILE} ${BASE_OUTPUT_DIR}/${TARGET_FULL_FILE}
+
+        echo "Remove old versions of $TARGET_DIR/$FULL_FILE"
+        hdfs dfs -rm $TARGET_DIR/$TARGET_FULL_FILE
+        echo "Push files ${FULL_FILE} to HDFS Directory: ${TARGET_DIR}"
+        hdfs dfs -put ${BASE_OUTPUT_DIR}/$TARGET_FULL_FILE $TARGET_DIR
+
+
+
+
+    fi
+done
+
---
+++ src/main/oozie.workflows/pig.hiveql/README.md
@@ -0,0 +1,5 @@
+# pig and hiveql
+
+This workflow is used to trigger a Pig Job that works on the data, staging it for further
+refinement by a HiveQL script.
+
