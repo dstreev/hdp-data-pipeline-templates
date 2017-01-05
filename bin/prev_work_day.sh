d8eb9a48fb78a51c84e38f4e1d32ef0e75f87b9e
David W. Streever <david@streever.com>
2017-01-04 20:20:26 -0500

    cleanup

--- bin/prev_work_day.sh
+++ bin/prev_work_day.sh
@@ -14,4 +14,23 @@ else
     RTN_DATE=$(date "--date=${IN_DATE} - 1 days" +%Y%m%d)
 fi
 
+# Check if RTN_DATE is a Holiday.  If it is, go back another day.
+
+if [ ! -f /etc/holidays ]; then
+  echo "Can't validate Holidays"
+  echo "Missing Holidays source file: /etc/holidays"
+  exit -1
+fi
+
+if grep -q $RTN_DATE /etc/holidays; then
+    DAY2=$(date "--date=${RTN_DATE}" +%a)
+
+    if [ "${DAY2}" == "Mon" ]; then
+        RTN_DATE=$(date "--date=${RTN_DATE} - 3 days" +%Y%m%d)
+    else
+        RTN_DATE=$(date "--date=${RTN_DATE} - 1 days" +%Y%m%d)
+    fi
+
+fi
+
 echo $RTN_DATE
